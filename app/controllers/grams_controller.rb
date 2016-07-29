3dd44e40615cab96b03cfd652bbc936d66b2ce41
vagrant <vagrant@precise32.(none)>
2016-07-29 20:05:24 +0000

    Lesson 18: Support Create Comments on Grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -58,9 +58,7 @@ class GramsController < ApplicationController
         params.require(:gram).permit(:message, :picture)
     end
 
-    def render_not_found
-        render text: 'Not Found :(', status: :not_found
-    end
+    
 
     def render_not_found(status=:not_found)
         render text: "#{status.to_s.titleize} :(", status: status
--- app/models/gram.rb
+++ app/models/gram.rb
@@ -2,6 +2,7 @@ class Gram < ActiveRecord::Base
     mount_uploader :picture, PictureUploader
     validates :picture, presence: true
     validates :message, presence: true
+    has_many :comments
 
     belongs_to :user
 end
